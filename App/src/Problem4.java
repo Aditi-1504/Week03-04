fd94c458e873c1668d1e4c6dd5eb466319b4802e
Aditi-1504 <aditiag150406@gmail.com>
2026-03-30 15:42:22 +0530

    Aditi added problem 4

---
+++ App/src/Problem4.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+class Asset {
+    String name;
+    double returnRate;
+    double volatility;
+
+    public Asset(String name, double returnRate, double volatility) {
+        this.name = name;
+        this.returnRate = returnRate;
+        this.volatility = volatility;
+    }
+}
+
+public class Problem4 {
+
+    public static void mergeSort(Asset[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    public static void merge(Asset[] arr, int l, int m, int r) {
+        Asset[] temp = new Asset[r - l + 1];
+        int i = l, j = m + 1, k = 0;
+
+        while (i <= m && j <= r) {
+            if (arr[i].returnRate <= arr[j].returnRate) temp[k++] = arr[i++];
+            else temp[k++] = arr[j++];
+        }
+
+        while (i <= m) temp[k++] = arr[i++];
+        while (j <= r) temp[k++] = arr[j++];
+
+        for (int x = 0; x < temp.length; x++) {
+            arr[l + x] = temp[x];
+        }
+    }
+
+    public static void quickSort(Asset[] arr, int l, int r) {
+        if (l < r) {
+            int p = partition(arr, l, r);
+            quickSort(arr, l, p - 1);
+            quickSort(arr, p + 1, r);
+        }
+    }
+
+    public static int partition(Asset[] arr, int l, int r) {
+        Asset pivot = arr[r];
+        int i = l - 1;
+
+        for (int j = l; j < r; j++) {
+            if (arr[j].returnRate > pivot.returnRate ||
+                    (arr[j].returnRate == pivot.returnRate &&
+                            arr[j].volatility < pivot.volatility)) {
+                i++;
+                Asset temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        Asset temp = arr[i + 1];
+        arr[i + 1] = arr[r];
+        arr[r] = temp;
+
+        return i + 1;
+    }
+
+    public static void print(Asset[] arr) {
+        for (Asset a : arr) {
+            System.out.println(a.name + " " + a.returnRate);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Asset[] assets = {
+                new Asset("AAPL", 12, 5),
+                new Asset("TSLA", 8, 7),
+                new Asset("GOOG", 15, 4)
+        };
+
+        mergeSort(assets, 0, assets.length - 1);
+        print(assets);
+
+        quickSort(assets, 0, assets.length - 1);
+        print(assets);
+    }
+}
